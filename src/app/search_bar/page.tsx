02bc06ddd93b4058d5f690ffbccd2a99696babfc
Anca Negoiu <anegoiu@princeton.edu>
2024-12-03 23:35:54 -0800

    refactor Search Bar into its own component

--- src/app/search_bar/page.tsx
+++ src/app/search_bar/page.tsx
@@ -1,4 +1,4 @@
-import SearchBar from "@/app/ui/search_bar"
+import SearchBar from "@/app/ui/search_bar/search_bar"
 
 export default async function SearchPage() {
         return <div>
--- src/app/ui/index.tsx
+++ src/app/ui/index.tsx
@@ -2,16 +2,17 @@
 
 import { useState } from "react";
 import { Card, CardContent, Typography } from '@mui/material';
-import {RAGSearchResults} from '@/utils'
+import { RAGSearchResults } from '@/utils'
 import styles from '@/app/ui/index.module.css'
 import ReactMarkdown from 'react-markdown';
+import SearchBar from "./search_bar/search_bar";
 
 
 export default function Home() {
     const [query, setQuery] = useState("");
     const [completion, setCompletion] = useState("");
 
-    const handleSearch = async () => {
+    const handleSearch = async (query: string) => {
         console.log(`Query ${query}`)
         const message = await (RAGSearchResults(query))
         message ? setCompletion(message) : console.log("We got an empty response from RAG")
@@ -19,27 +20,16 @@ export default function Home() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.searchBox}>
-                <h3>Apolitical Rag</h3>
-                <br></br>
-                <h4>Ask a question about the election and we'll generate a response that captures republican & democratic opinions</h4>
-                <input
-                    type="text"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    placeholder="Search..."
-                    className={styles.input}
-                />
-                <button onClick={handleSearch} className={styles.button}>Search</button>
-            </div>
+            <SearchBar
+                handleSearch={handleSearch} />
             {completion && (
                 <div className={styles.completionContainer}>
                     <Typography variant="h5" component="div">
-                    RAG Answer:
+                        RAG Answer:
                     </Typography>
                     <Card>
                         <CardContent>
-                           <ReactMarkdown>{completion}</ReactMarkdown>
+                            <ReactMarkdown>{completion}</ReactMarkdown>
                         </CardContent>
                     </Card>
                 </div>
