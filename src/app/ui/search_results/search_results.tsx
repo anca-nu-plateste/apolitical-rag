9c7d0492ae6ce1b0ee85f29290b938688765840c
Anca Negoiu <anegoiu@princeton.edu>
2024-12-04 22:24:55 -0800

    render artivle bodies in search resutls

--- src/app/ui/search_results/search_results.tsx
+++ src/app/ui/search_results/search_results.tsx
@@ -1,12 +1,14 @@
+import styles from '@/app/ui/search_results/search_results.module.css'
+
 export default function SearchResults({results}) {
     return (
-        <><p>Hi Mamas</p>
-        {results.map(result => (
-            <div key={result.url}> 
-                {/* TODO: read nody properly */}
-                <p>{result.body}</p>
-            </div>
-        ))}
-        </>
+        <div className={styles.results} >
+            {results.map(result => (
+                <div className={styles.results} key={result.url}> 
+                    {/* TODO: read body property */}
+                    <p>{result.text}</p>
+                </div>
+            ))}
+        </div>
     );
 }
