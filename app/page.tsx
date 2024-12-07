f0fbc44497588fd190c8684a763c4011ef0a0ea4
Anca Negoiu <negoiuanca@gmail.com>
2024-12-07 15:37:28 -0800

    Revert "try to narrow down problem"
    This reverts commit e07a5d05b09e093de07207f81618491ef121274b.

--- app/page.tsx
+++ app/page.tsx
@@ -4,44 +4,42 @@ import { useState } from "react";
 import { searchResults, RAGResponse } from '../utils'
 import styles from './ui/index.module.css'
 import SearchBar from "./search_bar/search_bar";
-// import LLMResponse from "./llm_response/llm_response";
-// import SearchResults from "./search_results/search_results";
+import LLMResponse from "./llm_response/llm_response";
+import SearchResults from "./search_results/search_results";
 import {SearchResult, Search} from './types'
 
 export default function Home() {
     const [completion, setCompletion] = useState("")
-    // const [results, setResults] = useState<SearchResult[]>([]);
+    const [results, setResults] = useState<SearchResult[]>([]);
     const [view, setView] = useState("search")
 
     const handleSearch = async (query: string) => {
         console.log(`Query ${query}`)
         const [blue_search, red_search] = await searchResults(query);
         const formattedResults = formatSearches(blue_search, red_search)
-        console.log(formattedResults)
-        // setResults(formattedResults)
+        setResults(formattedResults)
 
         const message = await RAGResponse(query, blue_search, red_search);
         if (message) {
             setCompletion(message)
-            console.log(completion)
         }
         setView("results")
     }
 
-    // const handleBack = () => {
-    //     setView("search")
-    // }
+    const handleBack = () => {
+        setView("search")
+    }
 
     return (
         <div className={styles.container}>
             {view == "search"?
             <SearchBar handleSearch={handleSearch} />:
                 <>
-                    {/* {completion && <LLMResponse response={completion}/>} */}
+                    {completion && <LLMResponse response={completion}/>}
                     <div>
                         <p> -------------</p>
                     </div>
-                    {/* {results != null && <SearchResults results={results} handleSearch={handleBack} />} */}
+                    {results != null && <SearchResults results={results} handleSearch={handleBack} />}
                 </>
             }    
         </div>
