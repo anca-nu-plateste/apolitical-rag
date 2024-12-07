ee26aeaa63759a7ded1cb84981959601ff8e7c18
Anca Negoiu <negoiuanca@gmail.com>
2024-12-07 14:02:18 -0800

    Revert "remove some more code"
    This reverts commit 6d83efdf942ef1752b7c0f01ef12195a9a7cbe3e.

--- app/llm_response/llm_response.tsx
+++ app/llm_response/llm_response.tsx
@@ -1,26 +1,26 @@
-// import { Card, CardContent, Typography } from '@mui/material';
-// import ReactMarkdown from 'react-markdown';
+import { Card, CardContent, Typography } from '@mui/material';
+import ReactMarkdown from 'react-markdown';
 
 
-// import styles from '@/app/ui/llm_response/llm_response.module.css'
+import styles from '@/app/ui/llm_response/llm_response.module.css'
 
 
-// interface LLMResponseProps {
-//     response: string;
-// }
+interface LLMResponseProps {
+    response: string;
+}
 
 
-// export default function LLMResponse({ response }: LLMResponseProps) {
-//     return (
-//         <div className={styles.completionContainer}>
-//             <Typography variant="h5" component="div">
-//                 Shortened Bias Evaluation
-//             </Typography>
-//             <Card className={styles.LLMResponse}>
-//                 <CardContent>
-//                     <ReactMarkdown>{response.slice(0, 800)}</ReactMarkdown>
-//                 </CardContent>
-//             </Card>
-//         </div>
-//     );
-// }
+export default function LLMResponse({ response }: LLMResponseProps) {
+    return (
+        <div className={styles.completionContainer}>
+            <Typography variant="h5" component="div">
+                Shortened Bias Evaluation
+            </Typography>
+            <Card className={styles.LLMResponse}>
+                <CardContent>
+                    <ReactMarkdown>{response.slice(0, 800)}</ReactMarkdown>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
